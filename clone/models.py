fb9bb335f355d708b598643a70bf50290230ba9b
karobia001 <karobiamaina81@gmail.com>
2019-11-15 16:28:39 +0300

    creation of our post class that will take in the image author etc.

--- clone/models.py
+++ clone/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, null=True)
+    caption = models.TextField()
+    posted_on = models.DateTimeField(default=timezone.now)
+    
+
