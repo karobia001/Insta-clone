58b563d74725586fecfea0f226f068291f9a06ff
karobia001 <karobiamaina81@gmail.com>
2019-11-17 15:48:04 +0300

    creation of an image class that takes in all objects of our class image

--- clone/models.py
+++ clone/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 # Create your models here.
 class Profile(models.Model):
-    profile =ImageField(blank=True,)
+    profile_picture =ImageField(blank=True,)
     bio = HTMLField()
     user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
 
@@ -24,3 +24,39 @@ class Profile(models.Model):
     def filter_by_id(cls, id):
         profile = Profile.objects.filter(user = id).first()
         return profile
+    
+    
+class Image(models):
+    # image_pic = models.ImageField(upload_to = 'p/', default='Image')
+    photo = ImageField(blank=True)
+    image_name = models.CharField(max_length = 50)
+    image_caption = HTMLField(blank=True)
+    post_date = models.DateTimeField(auto_now=True)
+    likes = models.BooleanField(default=False)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-post_date',)
+
+    def save_image(self):
+        self.save()
+    
+    @classmethod
+    def update_caption(cls, update):
+        pass
+    
+    @classmethod
+    def get_image_id(cls, id):
+        image = Image.objects.get(pk=id)
+        return image
+    
+    @classmethod
+    def get_profile_images(cls, profile):
+        images = Image.objects.filter(profile__pk = profile)
+        return images
+    
+    @classmethod
+    def get_all_images(cls):
+        images = Image.objects.all()
+        return images
+        
