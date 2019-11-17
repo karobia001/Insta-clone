7333b78858a580ccc2d92f2ef30bd93a42464e1e
karobia001 <karobiamaina81@gmail.com>
2019-11-17 15:36:10 +0300

    creation of profile that will take in our profile picture

--- clone/models.py
+++ clone/models.py
@@ -2,12 +2,25 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class Profile(models.Model):
+    profile =ImageField(blank=True,)
+    bio = HTMLField()
+    user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
 
-
-class Post(models.Model):
-    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
-    image = models.ImageField(blank=True, null=True)
-    caption = models.TextField()
-    posted_on = models.DateTimeField(default=timezone.now)
+    def save_profile(self):
+        self.save()
     
+    @classmethod
+    def search_profile(cls, name):
+        profile = Profile.objects.filter(user__username__icontains = name)
+        return profile
+    
+    @classmethod
+    def get_by_id(cls, id):
+        profile = Profile.objects.get(user = id)
+        return profile
 
+    @classmethod
+    def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user = id).first()
+        return profile
