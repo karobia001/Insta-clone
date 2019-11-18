f50e271c95a76ec1dab78603f56ede0df90f5bd6
karobia001 <karobiamaina81@gmail.com>
2019-11-18 08:07:48 +0300

    makes new class models that fit in well with our django 1

--- clone/models.py
+++ clone/models.py
@@ -1,80 +1,74 @@
+from django.contrib.auth.models import User
 from django.db import models
-from django.utils import timezone
 from tinymce.models import HTMLField
-from pyuploadcare.dj.models import ImageField
+
 # Create your models here.
+    
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    email = models.EmailField()
-    phone_number = PhoneNumberField(max_length=10, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    profile_pic = models.ImageField(upload_to = 'photos/',blank=True)
-    
-    
-    User.profile = property(lambda u: Profile.objects.get_or_create(user=u)[0])
+    profile_photo = models.ImageField('profile/', null = True)
+    bio = models.TextField()
+    user = models.ForeignKey(User)
+    last_update = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        ordering = ['-last_update']
 
     def __str__(self):
         return self.user.username
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
+    def save_profile(self):
+        self.save()
 
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-
-    instance.profile.save()
-
-@classmethod
-def get_other_userprofiles(cls,user_id):
-    profiles = Profile.objects.all()
-    other_userprofiles = []
-    for profile in profiles:
-        if profile.user_id !=user_id:
-             other_userprofiles.append(profile)
-    return other_userprofiles
+    def delete_profile(self):
+        Profile.objects.all().delete()        
+  
 
 
-def generate_id():
-        n = 10
-        random = str.ascii_uppercase + str.ascii_lowercase + str.digits
-        return ''.join(choice(random) for _ in range(n))
-
-
-class Posts(VoteModel,models.Model):
-    image = models.ImageField(upload_to = 'photos/',blank=True)
-    post_date = models.DateTimeField(auto_now_add = True)
-    description = models.TextField(max_length=500, blank=True)
+class Image(models.Model):
+    image = models.ImageField(upload_to ='image/', null=True)
+    image_name = models.CharField(max_length =30, null=True)
+    image_caption = models.TextField(null=True)
     user = models.ForeignKey(User)
-    location = models.CharField(max_length=30, blank=True)
-    slug = models.SlugField(max_length=10,unique=True, default=generate_id)
-    upvote_count = models.PositiveIntegerField(default=0)
-    downvote_count = models.PositiveIntegerField(default=0)
+    profile = models.ForeignKey(Profile)
+    likes = models.IntegerField(default=0)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.description
+
 
     class Meta:
-        ordering = ['-post_date']
+        ordering = ['-pub_date']
 
-    def get_absolute_url(self):
-        return reverse('posts:view', kwargs={'slug': self.slug})
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()    
 
     @classmethod
-    def get_posts(cls):
-        post = Posts.objects.all()
+    def search_by_user(cls,search_term):
+        images = cls.objects.filter(image_caption__icontains=search_term)
+        return images
 
-        return post
     @classmethod
-    def get_single_post(cls, pk):
-        post = cls.objects.get(pk=pk)
-        return post
+    def get_image_by_id(cls, image_id):
+        images = cls.objects.get(id=image_id)
+        return images   
 
-    @property
-    def image_url(self):
-        if self.image and hasattr(self.image, 'url'):
-            return self.image.url
+
+
+class Comment(models.Model):
+    comment = models.CharField(null = True, max_length= 5000, verbose_name = 'name')
+    date = models.DateTimeField(auto_now_add=True, null=True)
+    user = models.ForeignKey(User, null=True)
+    image = models.ForeignKey(Image, null= True)
+
+    class Meta:
+        verbose_name = "comments"
+        verbose_name_plural = "comments"
+        ordering = ['-date']        
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
