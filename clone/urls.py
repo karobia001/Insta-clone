54394d12a091e7e151e2610f8f1aabcb0bbc9d74
karobia001 <karobiamaina81@gmail.com>
2019-11-18 18:22:57 +0300

    adds all the rest of the forgoten files and deals with debbuggging errors

--- clone/urls.py
+++ clone/urls.py
@@ -9,7 +9,13 @@ urlpatterns=[
     url(r'^$', views.timeline, name='allTimelines'),
     url(r'^profile/', views.find_profile, name='find_profile'),
     url(r'^image/(\d+)', views.single_image, name='singleImage'),
-   
+    url(r'^user/(\d+)', views.user_profile, name='userProfile'),
+    url(r'^single_image/likes/(\d+)', views.single_image_like, name="singleImageLike"),
+    url(r'^profile/', views.find_profile, name="findProfile"),
+    url(r'^new/comment/(?P<username>[-_\w.]+)/$', views.new_comment, name="newComment"),
+    url(r'^new/status/(?P<username>[-_\w.]+)/$', views.new_post, name="new_status"),
+    url(r'^accounts/profile/', views.profile, name="myProfile"),        
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
